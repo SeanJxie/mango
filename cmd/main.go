91937a06f2f3bc15e4a7e8666aafe057fbf91f40
Sean Xie <seanjrnxie@gmail.com>
2026-03-25 14:40:15 -0400

    rename to favourite fruit

--- cmd/main.go
+++ cmd/main.go
@@ -3,55 +3,152 @@ package main
 import (
 	"math/rand/v2"
 
-	"github.com/SeanJxie/trace/trace"
+	mango "github.com/SeanJxie/mango/src"
 )
 
 func main() {
-	world := make([]trace.Shape, 0)
+	scene := 4
+	switch scene {
+	case 1:
+		scene1()
+	case 2:
+		scene2()
+	case 3:
+		scene3()
+	case 4:
+		scene4()
+	}
+}
 
-	checkerTex := trace.NewCheckeredTextureCol(0.32, trace.RGB{R: 0.2, G: 0.3, B: 0.1}, trace.RGB{R: 0.9, G: 0.9, B: 0.9})
-	ground_mat := trace.Lambertian{Albedo: checkerTex}
-	world = append(world, trace.NewSphere(trace.Vector3{X: 0, Y: -1000, Z: 0}, 1000, ground_mat))
+func scene1() {
+	world := make([]mango.Shape, 0)
+
+	checkerTex := mango.NewCheckeredTextureCol(0.32, mango.RGB{R: 0.2, G: 0.3, B: 0.1}, mango.RGB{R: 0.9, G: 0.9, B: 0.9})
+	ground_mat := mango.Lambertian{Albedo: checkerTex}
+	world = append(world, mango.NewSphere(mango.Vector3{X: 0, Y: -1000, Z: 0}, 1000, ground_mat))
 
 	for a := -11; a < 11; a++ {
 		for b := -11; b < 11; b++ {
-			center := trace.Vector3{X: float64(a) + 0.9*rand.Float64(), Y: 0.2, Z: float64(b) + 0.9*rand.Float64()}
+			center := mango.Vector3{X: float64(a) + 0.9*rand.Float64(), Y: 0.2, Z: float64(b) + 0.9*rand.Float64()}
 
-			if trace.Length3(trace.Subtract3(center, trace.Vector3{X: 4, Y: 0.2, Z: 0})) > 0.9 {
-				var sphere_mat trace.Material
+			if mango.Length3(mango.Subtract3(center, mango.Vector3{X: 4, Y: 0.2, Z: 0})) > 0.9 {
+				var sphere_mat mango.Material
 
-				albedo := trace.ScaleComponents(
-					trace.RGB{R: rand.Float64(), G: rand.Float64(), B: rand.Float64()},
-					trace.RGB{R: rand.Float64(), G: rand.Float64(), B: rand.Float64()},
+				albedo := mango.ScaleComponentsRGB(
+					mango.RGB{R: rand.Float64(), G: rand.Float64(), B: rand.Float64()},
+					mango.RGB{R: rand.Float64(), G: rand.Float64(), B: rand.Float64()},
 				)
-				sphere_mat = trace.Lambertian{Albedo: trace.NewSolidColourTextureAlbedo(albedo)}
-				world = append(world, trace.NewSphere(center, 0.2, sphere_mat))
+				sphere_mat = mango.PerfectMirror{Albedo: mango.NewSolidColourTextureAlbedo(albedo)}
+				world = append(world, mango.NewSphere(center, 0.2, sphere_mat))
 			}
 		}
 	}
 
-	mat2 := trace.Lambertian{Albedo: trace.NewSolidColourTextureAlbedo(trace.RGB{R: 0.4, G: 0.2, B: 0.1})}
-	world = append(world, trace.NewSphere(trace.Vector3{X: -4, Y: 1, Z: 0}, 1.0, mat2))
+	mat2 := mango.PerfectMirror{Albedo: mango.NewSolidColourTextureAlbedo(mango.RGB{R: 0.4, G: 0.2, B: 0.1})}
+	world = append(world, mango.NewSphere(mango.Vector3{X: -4, Y: 1, Z: 0}, 1.0, mat2))
 
-	bvh := trace.NewBVH(world, 0, len(world))
+	bvh := mango.BuildBVH(world, 0, len(world))
 
 	// Set up camera
-	lookFrom := trace.Vector3{X: 13, Y: 2, Z: 3}
-	lookAt := trace.Vector3{X: 0, Y: 0, Z: 0}
-	camera := trace.NewPerspectiveCamera(lookFrom, lookAt, 1000.0/740.0, 20, 0, 10)
+	lookFrom := mango.Vector3{X: 13, Y: 2, Z: 3}
+	lookAt := mango.Vector3{X: 0, Y: 0, Z: 0}
+	camera := mango.NewPerspectiveCamera(lookFrom, lookAt, 1000.0/740.0, 20, 0, 10)
 
-	imageBuffer := trace.NewImageBuffer(1000, 740)
 	samplesPerPixel := 100
+	imageBuffer := mango.NewImageBuffer(1000, 740, samplesPerPixel)
 
-	integrator := trace.PathIntegrator{
+	integrator := mango.PathIntegrator{
 		World:      bvh,
 		Camera:     camera,
-		Sampler:    &trace.UniformSampler{Spp: samplesPerPixel},
+		Sampler:    &mango.UniformSampler{Spp: samplesPerPixel},
 		Buffer:     imageBuffer,
 		MaxBounces: 50,
 	}
 
-	integrator.SlowRender()
-	imageBuffer.WriteToDisk(float64(samplesPerPixel))
+	integrator.TileRenderProgressiveParallel(32)
+	//integrator.ScanlineRenderParallel()
+	imageBuffer.Output("out.png")
+}
 
+func scene2() {
+	world := make([]mango.Shape, 0)
+	triangles := mango.ParseOBJ("./lucy.obj")
+
+	for _, t := range triangles {
+		world = append(world, t)
+	}
+
+	bvh := mango.BuildBVH(world, 0, len(world))
+
+	// Set up camera
+	lookFrom := mango.Vector3{X: 10, Y: 10, Z: 25}
+	lookAt := mango.Vector3{X: 0, Y: 0.5, Z: 0}
+	camera := mango.NewPerspectiveCamera(lookFrom, lookAt, 1, 20, 0, 10)
+
+	samplesPerPixel := 100
+	imageBuffer := mango.NewImageBuffer(1000, 1000, samplesPerPixel)
+
+	integrator := mango.PathIntegrator{
+		World:      bvh,
+		Camera:     camera,
+		Sampler:    &mango.UniformSampler{Spp: samplesPerPixel},
+		Buffer:     imageBuffer,
+		MaxBounces: 50,
+	}
+
+	integrator.TileRenderProgressiveParallel(32)
+	imageBuffer.Output("out.png")
+}
+
+func scene3() {
+	world := make([]mango.Shape, 0)
+	bvh := mango.BuildBVH(world, 0, len(world))
+
+	lookFrom := mango.Vector3{X: 0, Y: 0, Z: 0}
+	lookAt := mango.Vector3{X: 0, Y: 0, Z: 1}
+	camera := mango.NewPerspectiveCamera(lookFrom, lookAt, 1, 20, 0, 10)
+
+	samplesPerPixel := 100
+	imageBuffer := mango.NewImageBuffer(1000, 1000, samplesPerPixel)
+
+	integrator := mango.PathIntegrator{
+		World:      bvh,
+		Camera:     camera,
+		Sampler:    &mango.UniformSampler{Spp: samplesPerPixel},
+		Buffer:     imageBuffer,
+		MaxBounces: 50,
+	}
+
+	integrator.TileRenderProgressiveParallel(32)
+	imageBuffer.Output("out.png")
+}
+
+func scene4() {
+	world := make([]mango.Shape, 0)
+	triangles := mango.ParseOBJ("./suzanne.obj")
+
+	for _, t := range triangles {
+		world = append(world, t)
+	}
+
+	bvh := mango.BuildBVH(world, 0, len(world))
+
+	// Set up camera
+	lookFrom := mango.Vector3{X: 0, Y: 0, Z: 10}
+	lookAt := mango.Vector3{X: 0, Y: 0, Z: 0}
+	camera := mango.NewPerspectiveCamera(lookFrom, lookAt, 1, 20, 0, 10)
+
+	samplesPerPixel := 100
+	imageBuffer := mango.NewImageBuffer(500, 500, samplesPerPixel)
+
+	integrator := mango.PathIntegrator{
+		World:      bvh,
+		Camera:     camera,
+		Sampler:    &mango.UniformSampler{Spp: samplesPerPixel},
+		Buffer:     imageBuffer,
+		MaxBounces: 50,
+	}
+
+	integrator.ScanlineRenderParallel()
+	imageBuffer.Output("out.png")
 }
